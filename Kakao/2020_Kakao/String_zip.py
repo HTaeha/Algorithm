47d1fa3f93683b1a64b43087f3722b1528110717
HTaeha <xogk39@gmail.com>
2020-08-26 15:59:38 +0900

    kakao question added

---
+++ Kakao/2020_Kakao/String_zip.py
@@ -0,0 +1,11 @@
+# 2020 KAKAO BLIND REQRUITMENT
+# 문자열 압축
+# https://programmers.co.kr/learn/courses/30/lessons/60057?language=python3
+
+def solution(s):
+    answer = 0
+    return answer
+
+if __name__ == "__main__":
+    s = "aabbaccc"
+    solution(s)
