2add97998595417a5943ce671fed1045472f3793
HTaeha <xogk39@gmail.com>
2020-08-29 12:29:51 +0900

    Search_song added

--- Kakao/2020_Kakao/Search_song.py
+++ Kakao/2020_Kakao/Search_song.py
@@ -1,16 +1,17 @@
 class Node(object):
-    def __init__(self, data):
+    def __init__(self, data, depth):
         self.data = data
         self.next = set()
         self.is_terminal = False
+        self.depth = depth
 
 class Trie(object):
     def __init__(self):
-        self.root = Node("")
+        self.root = Node("", 0)
 
     def insert(self, s):
         node = self.root
-        for char in s:
+        for i, char in enumerate(s):
             found_char_flag = False
             for child in node.next:
                 if child.data == char:
@@ -19,26 +20,36 @@ class Trie(object):
                     break
 
             if not found_char_flag:
-                new_node = Node(char)
+                new_node = Node(char, i+1)
                 node.next.add(new_node)
                 node = new_node
 
         node.is_terminal = True
 
     def search(self, s):
+        count = 0
+        char_idx = 0
         node = self.root
-        for char in s:
-            found_char_flag = False
-            for child in node.next:
-                if child.data == char:
-                    found_char_flag = True
-                    node = child
-                    break
+        queue = []
+        queue.append(node)
+        while len(queue):
+            current = queue.pop(0)
+            char_idx = current.depth
 
-            if not found_char_flag:
-                return False
+            if char_idx == len(s): 
+                if current.is_terminal:
+                    count += 1
+                continue
 
-        return True
+            if s[char_idx] == '?':
+                for child in current.next:
+                    queue.append(child)
+            else:
+                for child in current.next:
+                    if s[char_idx] == child.data:
+                        queue.append(child)
+
+        return count
 
     def print_node(self):
         node = self.root
@@ -47,23 +58,28 @@ class Trie(object):
         queue.append(node)
         while len(queue):
             current = queue.pop(0)
-            print("depth : ", depth)
             for child in current.next:
+                print("depth : ", child.depth)
                 print(child.data)
                 queue.append(child)
 
             depth += 1
 
-if __name__ == "__main__":
+def solution(words, queries):
+    answer = []
+
     t = Trie()
+    for word in words:
+        t.insert(word)
 
-    t.insert("apple")
-    t.insert("approach")
+    for query in queries:
+        answer.append(t.search(query))
 
-    t.print_node()
+    return answer
 
-    print(t.search("apple"))
-    print(t.search("abc"))
-    print(t.search("approach"))
-    t.insert("abc")
-    print(t.search("abc"))
+if __name__ == "__main__":
+    words = ["frodo", "front", "frost", "frozen", "frame", "kakao"]
+    queries = ["fro??", "????o", "fr???", "fro???", "pro?"]
+
+    print(solution(words, queries))
+
