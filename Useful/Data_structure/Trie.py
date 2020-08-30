5620ef784d7e09dcb408b6e04ee65f0c1550c80b
HTaeha <xogk39@gmail.com>
2020-08-31 02:38:28 +0900

    Search_song completed & jinho's code added

--- Useful/Data_structure/Trie.py
+++ Useful/Data_structure/Trie.py
@@ -1,28 +1,39 @@
+# Trie에 들어가는 node class.
 class Node(object):
-    def __init__(self, data):
+    def __init__(self, data, depth):
+        # character 하나.
         self.data = data
+        # 다음 node를 저장하고 있음.
         self.next = set()
+        # 단어의 끝인지 판별.
         self.is_terminal = False
+        # 몇 번째 depth 인지 체크.
+        self.depth = depth
 
+# Trie class.
 class Trie(object):
     def __init__(self):
-        self.root = Node("")
+        # Root node. data가 없고 depth = 0.
+        self.root = Node("", 0)
 
     def insert(self, s):
         node = self.root
-        for char in s:
+        for i, char in enumerate(s):
             found_char_flag = False
+            # 이미 해당 node가 들어가 있으면 넘어감.
             for child in node.next:
                 if child.data == char:
                     found_char_flag = True
                     node = child
                     break
 
+            # 해당 node가 없으면 새로 생성.
             if not found_char_flag:
-                new_node = Node(char)
+                new_node = Node(char, i+1)
                 node.next.add(new_node)
                 node = new_node
 
+        # 마지막 node.
         node.is_terminal = True
 
     def search(self, s):
@@ -35,9 +46,11 @@ class Trie(object):
                     node = child
                     break
 
+            # 해당 node가 없으면 바로 False return.
             if not found_char_flag:
                 return False
 
+        # 끝까지 돌 때까지 false에 안 결렸으면 True ㄱㄷ셔구.
         return True
 
     def print_node(self):
