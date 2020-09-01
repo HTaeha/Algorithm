2cb061ebb89851d665514b227318171f8ed5b176
HTaeha <xogk39@gmail.com>
2020-09-02 02:43:53 +0900

    dart_game 다시 풀기

---
+++ Kakao/2018_Kakao/dart_game2.py
@@ -0,0 +1,11 @@
+# 다트 게임
+# https://programmers.co.kr/learn/courses/30/lessons/17682
+
+def solution(dartResult):
+    answer = 0
+
+    return answer
+
+if __name__ == "__main__":
+    dartResult = "1S2D*3T"
+    print(solution(dartResult))
