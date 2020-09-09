9196a0811fd937998a9fef93711b02eced4c38b8
HTaeha <xogk39@gmail.com>
2020-09-10 03:17:44 +0900

    셔틀버스 added

---
+++ Kakao/2018_Kakao/shuttle_bus2.py
@@ -0,0 +1,10 @@
+# 셔틀버스
+# https://programmers.co.kr/learn/courses/30/lessons/17678
+
+def solution(n, t, m, timetable):
+    answer = ''
+
+    return answer
+
+if __name__ == "__main__":
+    print(solution(n, t, m, timetable))
